e12e7cef95a62ac2b99549569ce1733a64619f3e
tfk-miza <mazensahloulitb@gmail.com>
2023-09-25 14:40:28 +0100

    start of the project beta

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart'; // Import the Firebase Realtime Database package
+import 'package:firebase_database/firebase_database.dart';
 import 'package:uuid/uuid.dart';
-import 'package:attendance_verification_appdemo/models/user_model.dart'; // Import your User model
+import 'package:attendance_verification_appdemo/models/user_model.dart';
+import 'package:http/http.dart' as http;
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
@@ -18,8 +20,12 @@ class RegistrationScreenState extends State<RegistrationScreen> {
   final TextEditingController prenomController = TextEditingController();
   final TextEditingController telController = TextEditingController();
 
-  final FirebaseDatabase database = FirebaseDatabase.instance; // Initialize the Firebase Realtime Database
+  final FirebaseDatabase database = FirebaseDatabase.instance;
   final Uuid uuid = const Uuid();
+
+  final RegExp emailRegExp = RegExp(
+    r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -27,95 +33,107 @@ class RegistrationScreenState extends State<RegistrationScreen> {
       appBar: AppBar(
         title: const Text('User Registration'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                controller: mailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: nomController,
-                decoration: const InputDecoration(labelText: 'Nom'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your last name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: prenomController,
-                decoration: const InputDecoration(labelText: 'Prénom'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your first name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: telController,
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(labelText: 'Téléphone'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your phone number';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    String userId = uuid.v4();
+      body: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: mailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    if (!emailRegExp.hasMatch(value)) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: nomController,
+                  decoration: const InputDecoration(labelText: 'Nom'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your last name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: prenomController,
+                  decoration: const InputDecoration(labelText: 'Prénom'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your first name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: telController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(labelText: 'Téléphone'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your phone number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      String userId = uuid.v4();
 
-                    User newUser = User(
-                      userId: userId,
-                      mail: mailController.text,
-                      nom: nomController.text,
-                      prenom: prenomController.text,
-                      tel: telController.text,
-                    );
+                      User newUser = User(
+                        userId: userId,
+                        mail: mailController.text,
+                        nom: nomController.text,
+                        prenom: prenomController.text,
+                        tel: telController.text,
+                      );
 
-                    // Store user data in the Firebase Realtime Database
-                    storeUserData(newUser);
+                      storeUserData(newUser);
+                      String message = "hello world";
+                      sendEmail(
+                        name: newUser.nom,
+                        email: newUser.mail,
+                        message: message,
+                      );
 
-                    mailController.clear();
-                    nomController.clear();
-                    prenomController.clear();
-                    telController.clear();
+                      mailController.clear();
+                      nomController.clear();
+                      prenomController.clear();
+                      telController.clear();
 
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('User registered successfully!'),
-                      ),
-                    );
-                  }
-                },
-                child: const Text('Register'),
-              ),
-            ],
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('User registered successfully!'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  void storeUserData(User user) {
-    DatabaseReference userRef = database.ref().child('users').child(user.userId);
+
+void storeUserData(User user) {
+    DatabaseReference userRef = database.ref().child('users').child(
+        user.userId);
 
     // Convert the User object to a Map
     Map<String, dynamic> userData = {
@@ -132,4 +150,36 @@ class RegistrationScreenState extends State<RegistrationScreen> {
       print('Error adding user data: $error');
     });
   }
+
+  Future sendEmail({
+    required String name,
+    required String email,
+    required String message,
+  }) async {
+    const serviceId = 'service_kpqwsps';
+    const templateId = 'template_5744d0s';
+    const userId = 'IANonE2Vf6_yPpF5s';
+
+    final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+    final response = await http.post(
+      url,
+      headers: {
+        'origin' : 'http://localhost',
+        'content-type': 'application/json',
+      },
+      body: json.encode({
+        'service_id': serviceId,
+        'template_id': templateId,
+        'user_id': userId,
+        'template_params': {
+          'to_name': name,
+          'to_mail': email,
+          'message': message,
+        },
+      }),
+    );
+    print(response.body);
+  }
+
+
 }
